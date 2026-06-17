a8d17f425f00256d48b648ae5734a6d0e26eada2
Sneha140529 <773snehag@gmail.com>
2026-06-17 23:54:47 +0530

    Add files via upload

---
+++ processes.js
@@ -0,0 +1,32 @@
+export const initialProcesses = [
+  {
+    id: 1,
+    arrival: 0,
+    burst: 4,
+    priority: 2,
+    remaining: 4,
+    finish: 0,
+    status: "new",
+    color: 0x60a5fa
+  },
+  {
+    id: 2,
+    arrival: 1,
+    burst: 2,
+    priority: 1,
+    remaining: 2,
+    finish: 0,
+    status: "new",
+    color: 0x34d399
+  },
+  {
+    id: 3,
+    arrival: 2,
+    burst: 3,
+    priority: 3,
+    remaining: 3,
+    finish: 0,
+    status: "new",
+    color: 0xfbbf24
+  }
+];
